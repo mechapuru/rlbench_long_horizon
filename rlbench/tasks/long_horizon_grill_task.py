62dd3d6cd14c69430bfdc5a8b4a9acc9aaa7899a
mechapuru <puru4robo@gmail.com>
2025-09-17 13:50:30 +0530

    going_till_grill

--- rlbench/tasks/long_horizon_grill_task.py
+++ rlbench/tasks/long_horizon_grill_task.py
@@ -11,32 +11,32 @@ class LongHorizonGrillTask(Task):
 
     def init_task(self) -> None:
         # Configure logging to write to a file
-        # logging.basicConfig(level=logging.INFO, 
-        #                     filename="output.txt", 
-        #                     filemode="w",
-        #                     format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.basicConfig(level=logging.INFO, 
+                            filename="output.txt", 
+                            filemode="w",
+                            format='%(asctime)s - %(levelname)s - %(message)s')
         logging.info('LongHorizonGrillTask: init_task called.')
 
-        # # Get handles to objects needed for this sub-task
-        # self.steak = Shape('steak')
-        # self.chicken = Shape('chicken')
-        # self.plate = Shape('plate')
-        # self.lid_joint = Joint('lid_joint')
+        # Get handles to objects needed for this sub-task
+        self.steak = Shape('steak')
+        self.chicken = Shape('chicken')
+        self.plate = Shape('plate')
+        self.lid_joint = Joint('lid_joint')
 
-        # self.success_sensor_grill = ProximitySensor('success_meat_on_grill')
-        # self.success_sensor_plate_source = ProximitySensor('success_source')
-        # self.success_sensor_plate_target = ProximitySensor('success_target')
+        self.success_sensor_grill = ProximitySensor('success_meat_on_grill')
+        self.success_sensor_plate_source = ProximitySensor('success_source')
+        self.success_sensor_plate_target = ProximitySensor('success_target')
 
-        # self.register_graspable_objects([self.steak, self.chicken, self.plate])
+        self.register_graspable_objects([self.steak, self.chicken, self.plate])
 
-        # self.register_success_conditions([
-        #     DetectedCondition(self.chicken, self.success_sensor_grill),
-        #     DetectedCondition(self.plate, self.success_sensor_plate_source, negated=True),
-        #     DetectedCondition(self.plate, self.success_sensor_plate_target),
-        #     JointCondition(self.lid_joint, np.deg2rad(50)),
-        #     NothingGrasped(self.robot.gripper)
-        # ])
-        # logging.info('Success conditions registered.')
+        self.register_success_conditions([
+            DetectedCondition(self.chicken, self.success_sensor_grill),
+            DetectedCondition(self.plate, self.success_sensor_plate_source, negated=True),
+            DetectedCondition(self.plate, self.success_sensor_plate_target),
+            JointCondition(self.lid_joint, np.deg2rad(50)),
+            NothingGrasped(self.robot.gripper)
+        ])
+        logging.info('Success conditions registered.')
 
 
     def init_episode(self, index: int) -> List[str]:
